f187fc10ddffeef719d793cb008f303de6656ab9
DK\msb <msb@sitecore.net>
2019-01-17 14:18:25 -0500

    Add retryer to verify connection. Configure retryer to send a message. Extend retryer to capture all TransportException

---
+++ src/Sitecore.Support.158128/Net/Smtp/ChilkatMessageTransport.cs
@@ -0,0 +1,178 @@
+﻿// © 2016 Sitecore Corporation A/S. All rights reserved.
+
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Chilkat;
+using Sitecore.Diagnostics;
+using Sitecore.EDS.Core.Dispatch;
+using Sitecore.EDS.Core.Exceptions;
+using Sitecore.EDS.Core.Net.Smtp;
+using Sitecore.Modules.EmailCampaign.Factories;
+using Debug = System.Diagnostics.Debug;
+using Task = System.Threading.Tasks.Task;
+
+namespace Sitecore.Support.EDS.Core.Net.Smtp
+{
+  /// <summary>
+  /// Defines chilkat message transport envelope.
+  /// </summary>
+  public class ChilkatMessageTransport : MessageTransport<ITransportClient>
+  {
+    private readonly int _maxTries;
+    private readonly int _delay;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChilkatMessageTransport" /> class.
+    /// </summary>
+    /// <param name="message">The email message.</param>
+    public ChilkatMessageTransport(EmailMessage message, int maxTries, int delay)
+        : base(message)
+    {
+      Email = ParseMessage(message);
+      _maxTries = maxTries;
+      _delay = delay;
+    }
+
+    /// <summary>
+    /// Gets the email message size.
+    /// </summary>
+    public override int Size
+    {
+      get { return Email.Size; }
+    }
+
+    /// <summary>
+    /// Gets the email.
+    /// </summary>
+    /// <value>
+    /// The email.
+    /// </value>
+    [NotNull]
+    protected Email Email { get; private set; }
+
+    /// <summary>
+    /// Sends the transport message asynchronously.
+    /// </summary>
+    /// <param name="client">The client.</param>
+    /// <returns>
+    /// The result of message dispatch operation <see cref="DispatchResult" />.
+    /// </returns>
+    public override Task<DispatchResult> SendAsync(ITransportClient client)
+    {
+      Assert.ArgumentNotNull(client, "client");
+      return SendTaskAsync(client);
+    }
+
+    /// <summary>
+    /// Sends the transport message asynchronously.
+    /// </summary>
+    /// <param name="client">The client.</param>
+    /// <returns>
+    /// The result of message dispatch operation <see cref="DispatchResult" />.
+    /// </returns>
+    private async Task<DispatchResult> SendTaskAsync(ITransportClient client)
+    {
+      Assert.ArgumentNotNull(client, "client");
+
+      var stopWatch = Stopwatch.StartNew();
+      await RetryOnFault(() => client.SendAsync(Email), _maxTries);
+      stopWatch.Stop();
+      var endTime = stopWatch.ElapsedMilliseconds;
+
+      var dispatchResult = new DispatchResult();
+      dispatchResult.Statistics.Add("SendingTime", endTime.ToString());
+      dispatchResult.Statistics.Add("Size", Size.ToString(CultureInfo.InvariantCulture));
+      return dispatchResult;
+    }
+
+    /// <summary>
+    /// Maps to chilkat email type.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    /// <returns>The email object for chilkat</returns>
+    private static Email ParseMessage(EmailMessage message)
+    {
+      var parsedEmail = new Email
+      {
+        Subject = message.Subject,
+        FromAddress = message.FromAddress,
+        FromName = message.FromName,
+        Charset = message.Charset,
+        Sender = message.ReturnPath,
+        BounceAddress = message.ReturnPath
+      };
+
+      if (message.ContentType == MessageContentType.Html)
+      {
+        parsedEmail.SetHtmlBody(message.HtmlBody);
+        if (!string.IsNullOrEmpty(message.PlainTextBody))
+        {
+          parsedEmail.AddPlainTextAlternativeBody(message.PlainTextBody);
+        }
+      }
+      else
+      {
+        parsedEmail.Body = message.PlainTextBody;
+      }
+
+      parsedEmail.AddHeaderField("X-Priority", ((int)message.Priority).ToString(CultureInfo.InvariantCulture));
+
+      foreach (string header in message.Headers)
+      {
+        parsedEmail.AddHeaderField(header, message.Headers[header]);
+      }
+
+      foreach (var recipient in message.Recipients)
+      {
+        parsedEmail.AddTo(string.Empty, recipient);
+      }
+
+      foreach (var attachment in message.Attachments.Where(a => !a.IsEmbedded))
+      {
+        parsedEmail.AddDataAttachment(attachment.Name, attachment.ByteContent);
+      }
+
+      foreach (var embeddeData in message.Attachments.Where(a => a.IsEmbedded && !string.IsNullOrEmpty(a.ContentId)))
+      {
+        var contentId = parsedEmail.AddRelatedData(embeddeData.Name, embeddeData.ByteContent);
+        parsedEmail.SetReplacePattern(embeddeData.ContentId, "cid:" + contentId);
+      }
+
+      return parsedEmail;
+    }
+
+    /// <summary>
+    /// The retry on fault.
+    /// </summary>
+    /// <param name="function">The function.</param>
+    /// <param name="maxTries">The max tries.</param>
+    /// <returns>
+    /// The <see cref="Task" />.
+    /// </returns>
+    private async Task RetryOnFault(Func<Task> function, int maxTries)
+    {
+      for (var i = 0; i < maxTries; i++)
+      {
+        try
+        {
+          await function().ConfigureAwait(false);
+          break;
+        }
+        // fix 158128
+        catch (TransportException)
+        {
+
+          if (i == maxTries - 1)
+          {
+            throw;
+          }
+          EcmFactory.GetDefaultFactory().Io.Logger.LogInfo("Sending operation is failed. Connection attempt: " + (i + 1) + ". It is retried.");
+          Thread.Sleep(_delay);
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.158128/Providers/SparkPost/Dispatch/DispatchProvider.cs
@@ -0,0 +1,104 @@
+﻿// © 2016 Sitecore Corporation A/S. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Sitecore.Diagnostics;
+using Sitecore.EDS.Core.Dispatch;
+using Sitecore.EDS.Core.Exceptions;
+using Sitecore.EDS.Core.Reporting;
+using Sitecore.EDS.Providers.SparkPost.Configuration;
+using Sitecore.EDS.Providers.SparkPost.Dispatch;
+using Sitecore.Modules.EmailCampaign.Factories;
+using Sitecore.StringExtensions;
+using Sitecore.Support.EDS.Core.Net.Smtp;
+
+namespace Sitecore.Support.EDS.Providers.SparkPost.Dispatch
+{
+  /// <summary>
+  /// Defines the DispatchProvider type.
+  /// </summary>
+  public class DispatchProvider : Sitecore.EDS.Providers.SparkPost.Dispatch.DispatchProvider
+  {
+    private readonly ConnectionPoolManager _connectionPoolManager;
+    private readonly IConfigurationStore _configurationStore;
+    private readonly string _returnPath;
+    private readonly int _maxTries;
+    private readonly int _delay;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DispatchProvider" /> class.
+    /// </summary>
+    /// <param name="connectionPoolManager">The connection pool manager.</param>
+    /// <param name="environmentIdentifier">The environment identifier</param>
+    /// <param name="configurationStore">The configuration store</param>
+    /// <param name="returnPath">Sets the return path address</param>
+    /// <param name="maxTries">Sets the max tries</param>
+    /// <param name="delay">Sets the delay</param>
+    /// 
+      // fix 304755
+    public DispatchProvider([NotNull] ConnectionPoolManager connectionPoolManager, [NotNull] IEnvironmentId environmentIdentifier, [NotNull] IConfigurationStore configurationStore, [NotNull] string returnPath, [NotNull]  string maxTries = "3", [NotNull]  string delay = "1000")
+        : base(connectionPoolManager, environmentIdentifier, configurationStore, returnPath)
+    {
+      Assert.ArgumentNotNull(connectionPoolManager, "connectionPoolManager");
+
+      _connectionPoolManager = connectionPoolManager;
+      _configurationStore = configurationStore;
+      _returnPath = returnPath;
+      _maxTries = Int32.Parse(maxTries);
+      _delay = Int32.Parse(delay);
+    }
+
+    /// <summary>
+    /// Validates that the SMTP connection can be established.
+    /// </summary>
+    /// <returns>
+    /// <c>true</c> if the SMTP connection can be established.
+    /// </returns>
+    public override async Task<bool> ValidateDispatchAsync()
+    {
+      // fix 304754
+      for (var i = 0; i < _maxTries; i++)
+      {
+
+        var client = await _connectionPoolManager.GetSmtpConnectionAsync();
+
+        // fix 158128
+        if (!client.ValidateSmtpConnection().Result)
+        {
+
+          if (i == _maxTries - 1)
+          {
+            return false;
+          }
+          EcmFactory.GetDefaultFactory().Io.Logger.LogInfo("Connection validation is failed. Connection attempt: " + (i + 1) + ". It is retried.");
+          Thread.Sleep(_delay);
+        }
+        else
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    /// <summary>
+    /// Sends the specified message.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    /// <returns>
+    /// The <see cref="DispatchResult"/>
+    /// </returns>
+    protected override async Task<DispatchResult> SendEmailAsync(EmailMessage message)
+    {
+      message.ReturnPath = _returnPath;
+
+      var chilkatMessageTransport = new ChilkatMessageTransport(message, _maxTries, _delay);
+      var client = await _connectionPoolManager.GetSmtpConnectionAsync();
+
+      return await chilkatMessageTransport.SendAsync(client);
+    }
+  }
+}
